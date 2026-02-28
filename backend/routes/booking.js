160683b28faeeacac3d378dab3bcff7395ce97b5
satheesan999 <rajasekarsatheesan2003@gmail.com>
2026-02-28 14:51:29 +0530

    add the updatebooking status function in the booking route.js

--- backend/routes/booking.js
+++ backend/routes/booking.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const { createBooking } = require("../controllers/bookingController");
+const { updateBookingStatus } = require("../controllers/bookingController"); 
+
 
 router.post("/", createBooking);
+router.put("/:id/status", updateBookingStatus);
 
 module.exports = router;
+
