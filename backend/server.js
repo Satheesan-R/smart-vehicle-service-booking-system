732533e5171f9db9e3d85decdbec21a8b1b4574f
satheesan999 <rajasekarsatheesan2003@gmail.com>
2026-03-01 14:32:04 +0530

    some grammer issue resolved

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,8 @@ app.use(cors());
 app.use(express.json());
 
 // Import routes
-const authRoutes = require("./routes/authRoutes");
-const bookingRoutes = require("./routes/bookingRoutes");
+const authRoutes = require("./routes/auth");
+const bookingRoutes = require("./routes/booking");
 
 app.use("/api/auth", authRoutes);
 app.use("/api/bookings", bookingRoutes);
